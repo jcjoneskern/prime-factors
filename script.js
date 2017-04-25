2000299126791d1ab29442fed70e55cf4a03a227
J-C <jcjoneskern@gmail.com>
2017-04-25 17:21:29 -0400

    changed to a while loop for more control over when i increments

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function factor(num) {
   let result = [];
   const factors = [2, 3, 5, 7];
+  let i = 0;
 
-  for(var i = 0; i < factors.length; i++) {
-    if (num % factors[i] === 0) { //if the number is divisible by any arbitrary number in the array
-      // okay but what if the number is a factor?
+  while (i < factors.length) {
+    if (num === factors[i]) {
+      result.push(num);
+      i++;
+    } else if (num % factors[i] === 0) {
       result.push(factors[i]);
       num /= factors[i];
-    } else if (/*???*/) {
-      // should push the number into the array once all the factors are out
-      result.push(num);
+    } else {
+      i++;
     }
   }
-
-  // I feel like I'm getting ahead of myself
 
 
   return result;
