56c8f2e2a709a206b1cfb2d44807707d0973566d
J-C <jcjoneskern@gmail.com>
2017-04-25 16:47:50 -0400

    got some test set up but...yikes

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function factor(num) {
-  return [num];
+  let result = [];
+  const factors = [2, 3, 5, 7];
+
+  for(var i = 0; i < factors.length; i++) {
+    if (num % factors[i] === 0) {
+      result.push(factors[i]);
+      num /= factors[i];
+    } else {
+      result.push(num);
+    }
+  }
+
+  // I feel like I'm getting ahead of myself
+
+
+  return result;
 }
 
 module.exports = factor;
--- spec/spec.js
+++ spec/spec.js
@@ -4,4 +4,16 @@ describe("factor-er", function() {
   it("factors 2", function() {
     expect(factor(2)).toEqual([2]);
   });
+  it("factors 3", function() {
+    expect(factor(3)).toEqual([3]);
+  });
+  it("factors 5", function() {
+    expect(factor(5)).toEqual([5]);
+  });
+  it("factors 7", function() {
+    expect(factor(7)).toEqual([7]);
+  });
+  it("factors 4", function() {
+    expect(factor(4)).toEqual([2,2]);
+  });
 });
